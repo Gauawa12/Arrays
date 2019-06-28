9684155504662b48571a26c30ce7c0480b882330
Gauawa12 <42520099+Gauawa12@users.noreply.github.com>
2019-06-28 22:46:57 +0530

    Observation,Qin

--- Trapping_Rain_Water.cpp
+++ Trapping_Rain_Water.cpp
@@ -1,11 +1,14 @@
-Given an array arr[] of N non-negative integers representing height of blocks at index i as Ai where the width of each block is 1. Compute how much water can be trapped in between blocks after raining.
+Given an array arr[] of N non-negative integers representing height of blocks at index i as Ai where the width of each 
+block is 1. Compute how much water can be trapped in between blocks after raining.
 Structure is like below:
 |  |
 |_|
 We can trap 2 units of water in the middle gap.
 
 Input:
-The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. Each test case contains an integer N denoting the size of the array, followed by N space separated numbers to be stored in array.
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows.
+ Each test case contains an integer N denoting the size of the array, followed by N space separated numbers to be stored 
+ in array.
 
 Output:
 Output the total unit of water trapped in between the blocks.
@@ -31,8 +34,15 @@ Output:
 0
 
 Explanation:
-Testcase 1: Water trapped by block of height 4 is 3 units, block of height 0 is 7 units. So, total unit of water trapped is 10 units.
+Testcase 1: Water trapped by block of height 4 is 3 units, block of height 0 is 7 units. So, total unit of water trapped 
+is 10 units.
  
+ 
+/*
+Think that whenever you are standing at a place why it will store water. 
+The difference in the length of walls on both sides will give the storage.
+Water will be stored only if the min(largest wall on left of i,largest wall on right of i)>wall at i.
+*/ 
 
 Sol : 
 int trappingWater(int a[], int n){
