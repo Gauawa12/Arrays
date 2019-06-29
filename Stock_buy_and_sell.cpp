7b36e5cabf1479f2d52c870cd71b944e0048c684
Gauawa12 <42520099+Gauawa12@users.noreply.github.com>
2019-06-29 11:45:02 +0530

    Important,Observation,Qin

--- Stock_buy_and_sell.cpp
+++ Stock_buy_and_sell.cpp
@@ -1,14 +1,17 @@
 
-The cost of stock on each day is given in an array A[] of size N. Find all the days on which you buy and sell the stock so that in between those days your profit is maximum.
+The cost of stock on each day is given in an array A[] of size N. Find all the days on which you buy and sell the stock so
+that in between those days your profit is maximum.
 
 Input: 
-First line contains number of test cases T. First line of each test case contains an integer value N denoting the number of days, followed by an array of stock prices of N days. 
+First line contains number of test cases T. First line of each test case contains an integer value N denoting the number
+of days, followed by an array of stock prices of N days. 
 
 Output:
 For each testcase, output all the days with profit in a single line. And if there is no profit then print "No Profit".
 
 User Task:
-The task is to complete the function stockBuySell() which finds the days of buying and selling stock and print them. The newline is automatically added by the driver code.
+The task is to complete the function stockBuySell() which finds the days of buying and selling stock and print them.
+    The newline is automatically added by the driver code.
 
 Constraints:
 1 <= T <= 100
@@ -36,7 +39,7 @@ Testcase 1: We can buy stock on day 0, and sell it on 3rd day, which will give u
 Note: Output format is as follows - (buy_day sell_day) (buy_day sell_day)
 
 
-/*
+
 
 
 
@@ -87,7 +90,7 @@ void stockBuySell(int a[], int n)
                          {
                              ans.push_back(make_pair(mini,i-1));
                          }
-                         while(v[i]==-1)
+                         while(v[i]==-1)//reach at last -1 for new transaction
                          {
                              i++;
                          }
@@ -110,5 +113,16 @@ void stockBuySell(int a[], int n)
     
     
 }
+/*
+Approach:
+Think of when you will want to sell. Suppose you have bought a stock and so you should sell it only when after increasing its 
+price will go down. So stock will be sold at all positions which are greater than its continuous right one . A new transaction 
+starts from here on with new min value at present.
+look out:
+if there are consecutive -1's(I stored -1 at places from where new transaction has to start,meaning which is lower than its
+previous one), then the transaction has to start from last -1 in continuity.
+if the prices are in increasing order only. Put -1 at last to depict end of transaction.
+If the prices are in decreasing order only . No profit.
 
+*/
 
